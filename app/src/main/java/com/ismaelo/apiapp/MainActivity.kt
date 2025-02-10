060e0ac9448db55e6c843708e5be55c1798938fa
ismaelo107 <ismaeloeg@iescastelar.com>
2025-02-10 12:06:30 +0100

    Add README para explicar la rama de estudio

--- app/src/main/java/com/ismaelo/apiapp/MainActivity.kt
+++ app/src/main/java/com/ismaelo/apiapp/MainActivity.kt
@@ -4,66 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.rememberDrawerState
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.compose.rememberNavController
-import com.ismaelo.apiapp.data.local.LocalDatasource
-import com.ismaelo.apiapp.navigation.AppNavigation
-import com.ismaelo.apiapp.navigation.navigationBar.DrawerContent
-import com.ismaelo.apiapp.navigation.navigationBar.TopNavigationBar
+import androidx.compose.ui.tooling.preview.Preview
 import com.ismaelo.apiapp.ui.theme.ApiAppTheme
-import com.ismaelo.apiapp.ui.view.component.AnimatedGradientBackground
-import com.ismaelo.apiapp.viewModel.MovieViewModel
-import com.ismaelo.apiapp.viewModel.MovieViewModelFactory
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    private lateinit var movieViewModel: MovieViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        val localDatasource = LocalDatasource(applicationContext)
-        val factory = MovieViewModelFactory(localDatasource)
-        movieViewModel = ViewModelProvider(this, factory)[MovieViewModel::class.java]
-
         setContent {
-            val navController = rememberNavController()
-            val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-            val scope = rememberCoroutineScope()
-
             ApiAppTheme {
-
-                ModalNavigationDrawer(drawerState = drawerState,
-                    drawerContent = { DrawerContent(navController) }) {
-                    Scaffold(topBar = {
-                        TopNavigationBar(
-                            onMenuClick = { scope.launch { drawerState.open() } },
-                        )
-                    }) { innerPadding ->
-                        AnimatedGradientBackground()
-                        Column(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(innerPadding)
-                        ) {
-
-                            AppNavigation(
-                                modifier = Modifier, movieViewModel, navController = navController
-                            )
-                        }
-                    }
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android", modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!", modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ApiAppTheme {
+        Greeting("Android")
     }
 }
