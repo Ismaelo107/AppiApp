95963f9664597ee2c5949ae945d5eb01262a14cb
ismaelo107 <ismaeloeg@iescastelar.com>
2025-02-14 16:16:41 +0100

    Inicializando rama ismael

--- app/src/main/java/com/ismaelo/apiapp/data/remote/RemoteDatasource.kt
+++ app/src/main/java/com/ismaelo/apiapp/data/remote/RemoteDatasource.kt
@@ -1,60 +1,9 @@
 package com.ismaelo.apiapp.data.remote
 
-import retrofit2.Response
-
-sealed class Result<out T> {
-    data class Success<out T>(val data: T) : Result<T>()
-    data class Error(val exception: Exception) : Result<Nothing>()
-}
-
-class EmptyResponseBodyException(message: String) : Exception(message)
-class NetworkException(message: String) : Exception(message)
-
 class RemoteDatasource(private val apiService: ApiService) {
 
-    suspend fun nowPlayingMovies(apiKey: String): Result<MovieResponse> {
-        return try {
-            val response = apiService.nowPlayingMovies(apiKey)
-            handleResponse(response)
-        } catch (e: Exception) {
-            Result.Error(e)
-        }
-    }
+    suspend fun getLastComic() = apiService.getLastComic()
 
-    suspend fun getPopularMovies(apiKey: String): Result<MovieResponse> {
-        return try {
-            val response = apiService.getPopular(apiKey)
-            handleResponse(response)
-        } catch (e: Exception) {
-            Result.Error(e)
-        }
-    }
+    suspend fun getComic(number: Int) = apiService.getComic(number)
 
-    suspend fun getTopRatedMovies(apiKey: String): Result<MovieResponse> {
-        return try {
-            val response = apiService.getTopRated(apiKey)
-            handleResponse(response)
-        } catch (e: Exception) {
-            Result.Error(e)
-        }
-    }
-
-    suspend fun getUpcomingMovies(apiKey: String): Result<MovieResponse> {
-        return try {
-            val response = apiService.getUpComing(apiKey)
-            handleResponse(response)
-        } catch (e: Exception) {
-            Result.Error(e)
-        }
-    }
-
-    private fun handleResponse(response: Response<MovieResponse>): Result<MovieResponse> {
-        return if (response.isSuccessful) {
-            response.body()?.let {
-                Result.Success(it)
-            } ?: Result.Error(EmptyResponseBodyException("Empty response body"))
-        } else {
-            Result.Error(NetworkException("Error: ${response.message()} (${response.code()})"))
-        }
-    }
 }
