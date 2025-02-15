f521f6f58ee7e6193d6ce2243a06d3477ed5cea1
ismaelo107 <ismaeloeg@iescastelar.com>
2025-02-15 22:01:39 +0100

    Appi App funcinal y se conecta con la api, corregido los errrore de intenter conectarme con el auth token en vez de la api_key, me ha costado mucho

--- app/src/main/java/com/ismaelo/apiapp/data/loca/LocalDatasource.kt
+++ app/src/main/java/com/ismaelo/apiapp/data/loca/LocalDatasource.kt
@@ -2,27 +2,28 @@ package com.ismaelo.apiapp.data.loca
 
 import android.content.Context
 import android.util.Log
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.stateIn
-
+import kotlinx.coroutines.withContext
 
 class LocalDatasource(applicationContext: Context) {
     private val db: AppDataBase = AppDataBase.getDatabase(applicationContext)
 
-    @OptIn(DelicateCoroutinesApi::class)
-    suspend fun getAll(): Flow<List<Comic>> {
-        return db.comicDao().getAll().stateIn(GlobalScope)
+    suspend fun getAll(): Flow<List<Movie>> {
+        return db.comicDao().getAll() // Retorna el Flow directamente sin necesidad de usar stateIn
     }
 
-    fun insert(comic: Comic) {
-        val result = db.comicDao().insert(comic)
-        Log.d("LOCALDS", "Resultado: $result")
+    suspend fun insert(movie: Movie) {
+        // Ejecuta la inserción en un hilo de fondo con withContext para asegurar que se ejecute correctamente.
+        withContext(Dispatchers.IO) {
+            val result = db.comicDao().insert(movie)
+            Log.d("LOCALDS", "Resultado: $result")
+        }
     }
 
-    fun delete(comic: Comic) {
-        db.comicDao().delete(comic)
+    suspend fun delete(movie: Movie) {
+        withContext(Dispatchers.IO) {
+            db.comicDao().delete(movie)
+        }
     }
-
 }
