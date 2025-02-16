20c6287a20ddf82bcfdfb87408f8a9efb014d981
ismaelo107 <ismaeloeg@iescastelar.com>
2025-02-16 11:18:16 +0100

    Cuando no hay conexión nos ofrece el botón para ver la base de datos

--- app/src/main/java/com/ismaelo/apiapp/ui/view/FavoritesScreen.kt
+++ app/src/main/java/com/ismaelo/apiapp/ui/view/FavoritesScreen.kt
@@ -1,12 +1,16 @@
-package com.ismaelo.apiapp.ui.view.component
+package com.ismaelo.apiapp.ui.view
+
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
@@ -19,9 +23,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,20 +30,50 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
-import com.ismaelo.apiapp.data.remote.MovieDTO
-import com.ismaelo.apiapp.data.remote.toLocalMovie
+import com.ismaelo.apiapp.data.local.Movie
 import com.ismaelo.apiapp.viewModel.MovieViewModel
 
-
 @Composable
-fun MovieCard(movie: MovieDTO, navController: NavHostController, movieViewModel: MovieViewModel) {
-    var isFavorite by remember { mutableStateOf(false) } // Estado del favorito
+fun FavoritesScreen(movieViewModel: MovieViewModel, navController: NavHostController) {
+    val favoriteMovies by movieViewModel.favoriteMovies.collectAsState()
 
-    val favorites = movieViewModel.favoriteMovies.collectAsState().value
-    LaunchedEffect(movie.id) {
-        isFavorite = favorites.any { it.id == movie.id }
+    LaunchedEffect(Unit) {
+        movieViewModel.fetchFavoriteMovies()
     }
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Favorite Movies",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        if (favoriteMovies.isEmpty()) {
+            Text("No favorites yet", style = MaterialTheme.typography.bodyMedium)
+        } else {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2), modifier = Modifier.fillMaxSize()
+            ) {
+                items(favoriteMovies.size) { index ->
+                    FavoriteMovieCard(
+                        movie = favoriteMovies[index],
+                        navController = navController,
+                        movieViewModel = movieViewModel
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FavoriteMovieCard(
+    movie: Movie, navController: NavHostController, movieViewModel: MovieViewModel
+) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -80,18 +111,13 @@ fun MovieCard(movie: MovieDTO, navController: NavHostController, movieViewModel:
 
             IconButton(
                 onClick = {
-                    if (isFavorite) {
-                        movieViewModel.deleteMovieFromLocal(movie.toLocalMovie())
-                    } else {
-                        movieViewModel.saveMovieToLocal(movie)
-                    }
-                    isFavorite = !isFavorite // Cambiar el estado de favorito
+                    movieViewModel.deleteMovieFromLocal(movie)
                 }, modifier = Modifier.size(40.dp)
             ) {
                 Icon(
                     imageVector = Icons.Default.Favorite,
-                    contentDescription = "Favorite",
-                    tint = if (isFavorite) Color.Red else Color.Gray
+                    contentDescription = "Remove Favorite",
+                    tint = Color.Red
                 )
             }
 
