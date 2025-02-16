f0df57c84aa26bb98b6f52d2025b0a962d728cc4
ismaelo107 <ismaeloeg@iescastelar.com>
2025-02-16 01:36:57 +0100

    Base de datos configurada y funcional

---
+++ app/src/main/java/com/ismaelo/apiapp/viewModel/MovieViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.ismaelo.apiapp.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.ismaelo.apiapp.data.local.LocalDatasource
+
+class MovieViewModelFactory(
+    private val localDatasource: LocalDatasource
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MovieViewModel::class.java)) {
+            return MovieViewModel(localDatasource) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
