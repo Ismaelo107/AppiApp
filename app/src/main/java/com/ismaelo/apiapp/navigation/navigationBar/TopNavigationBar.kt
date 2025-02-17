a2a0ac5cbff694fd4c3e80f00f2217e2567eb702
Ismaelo107 <96046256+Ismaelo107@users.noreply.github.com>
2025-02-17 20:48:59 +0100

    Cambios estéticos 1, desplegabla lateral...

--- app/src/main/java/com/ismaelo/apiapp/navigation/navigationBar/TopNavigationBar.kt
+++ app/src/main/java/com/ismaelo/apiapp/navigation/navigationBar/TopNavigationBar.kt
@@ -7,40 +7,17 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopNavigationBar(
     onMenuClick: () -> Unit
 ) {
-    TopAppBar(
-        title = {
-            Text(
-                text = "iMovies", // Cambiar el título a "iMovies"
-                color = Color.Cyan, // Texto en cian
-                fontSize = 24.sp, // Texto más grande
-                fontWeight = FontWeight.Bold // Texto más grueso
-            )
-        },
-        navigationIcon = {
-            IconButton(onClick = { onMenuClick() }) {
-                Icon(
-                    imageVector = Icons.Filled.Menu,
-                    contentDescription = "Menu",
-                    tint = Color.Cyan // Icono en cian
-                )
-            }
-        },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color.Transparent // Fondo transparente
-        ),
-        actions = {
-            // Aquí puedes agregar más acciones si es necesario
+
+    TopAppBar(title = { Text(text = "Movie") }, navigationIcon = {
+        IconButton(onClick = { onMenuClick() }) {
+            Icon(Icons.Filled.Menu, contentDescription = "Menu")
         }
-    )
+    })
 }
--- app/src/main/java/com/ismaelo/apiapp/ui/view/screens/UpComingScreen.kt
+++ app/src/main/java/com/ismaelo/apiapp/ui/view/screens/UpComingScreen.kt
@@ -1,45 +1,29 @@
-package com.ismaelo.apiapp.ui.view
+package com.ismaelo.apiapp.ui.view.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import coil.compose.rememberAsyncImagePainter
-import com.ismaelo.apiapp.data.remote.MovieDTO
-import com.ismaelo.apiapp.data.remote.toLocalMovie
 import com.ismaelo.apiapp.navigation.Destinations
 import com.ismaelo.apiapp.ui.view.component.MovieCard
 import com.ismaelo.apiapp.viewModel.MovieViewModel
+
 @Composable
-fun Popular(movieViewModel: MovieViewModel, navController: NavHostController) {
+fun UpComing(movieViewModel: MovieViewModel, navController: NavHostController) {
     val movies by movieViewModel.popularMovies.collectAsState()
     val isLoading by movieViewModel.isLoading.collectAsState()
     val isConnected by movieViewModel.isConnected.collectAsState() // Observamos el estado de conexión
@@ -53,7 +37,7 @@ fun Popular(movieViewModel: MovieViewModel, navController: NavHostController) {
 
     // Cargar las películas populares solo una vez
     LaunchedEffect(Unit) {
-        movieViewModel.fetchPopularMovies()
+        movieViewModel.fetchUpcomingMovies()
         movieViewModel.fetchFavoriteMovies()
     }
 
@@ -68,6 +52,7 @@ fun Popular(movieViewModel: MovieViewModel, navController: NavHostController) {
             isLoading -> {
                 CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
             }
+
             movies.isNotEmpty() -> {
                 LazyVerticalGrid(
                     columns = GridCells.Fixed(2), modifier = Modifier.fillMaxSize()
@@ -81,6 +66,7 @@ fun Popular(movieViewModel: MovieViewModel, navController: NavHostController) {
                     }
                 }
             }
+
             else -> {
                 Text("No movies available.", color = Color.Red)
                 Button(
